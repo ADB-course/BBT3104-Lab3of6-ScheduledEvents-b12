5f4af5f05f914a0ebfca6876fa20fff96e5186ba
ShirleenMuthoni1 <167191270+ShirleenMuthoni1@users.noreply.github.com>
2024-10-01 17:13:49 +0300

    Update lab_submission.sql
    Signed-off-by: ShirleenMuthoni1 <167191270+ShirleenMuthoni1@users.noreply.github.com>

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,8 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)
+SELECT AVG(TIMESTAMPDIFF(MINUTE, ticket_created_time, ticket_resolved_time))
+FROM customer_service_ticket
+WHERE ticket_created_time >= DATE_SUB(NOW(), INTERVAL 1 HOUR);
